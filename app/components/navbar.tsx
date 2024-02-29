d493f42812487da677f3fb819c46578352596b6d
Rayyan Khan <weatherwhiz06@gmail.com>
2024-02-29 17:47:25 -0500

    updated navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,22 +2,45 @@ import { Link } from '@remix-run/react';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white max-w-screen-xl flex flex-wrap flex-col items-center justify-between mx-auto p-4">
-      <div className="flex">
-        <Link to="/">NoorHadiaArts</Link>
+    <nav className="bg-gray-100 py-4">
+      <div className="container mx-auto flex justify-between items-center text-black font-serif">
+        {/*-- Logo Name */}
+        <Link to="/" className="text-2xl">
+          NoorHadiaArts
+        </Link>
+
+        {/* Navigation links */}
+        <ul className="flex space-x-12">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products" className="">
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="">
+              Contact
+            </Link>
+          </li>
+        </ul>
+
+        {/* Login & Cart */}
+        <ul className="flex space-x-12">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
       </div>
-      <ul className="flex flex-row justify-between">
-        <li className="px-3">
-          <Link className="" to="/products">
-            Shop
-          </Link>
-        </li>
-        <li className="px-3">
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './app/routes/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: { serif: ['georgia', 'ui-serif'] },
     extend: {},
   },
   plugins: [],
