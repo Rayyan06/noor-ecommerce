24e20b6ee1c65488dda15584bc073330fb4c80c7
Rayyan Khan <weatherwhiz06@gmail.com>
2024-03-01 18:53:20 -0500

    Route

--- app/routes/products/productCard.tsx
+++ app/routes/products/productCard.tsx
@@ -1,11 +1,12 @@
 type ProductCardProps = {
   name: string;
+  price: number;
 };
 export default function ProductCard({ name, price, image }: ProductCardProps) {
   return (
-    <div>
-      <h3>{name}</h3>
-      <h3>{price}</h3>
+    <div className="flex flex-col items-center justify-end">
+      <h3 className="text-xl font-serif ">{name}</h3>
+      <h3>${price}</h3>
     </div>
   );
 }
--- app/routes/products/route.tsx
+++ app/routes/products/route.tsx
@@ -2,9 +2,13 @@ import ProductCard from './productCard';
 
 export default function Products() {
   return (
-    <div className="grid h-screen">
-      <h1>All Products</h1>
-      <ProductCard name={'Wood Carving'} price={100} />
+    <div className="grid grid-cols-3 gap-3 justify-center h-screen overflow-auto">
+      <ProductCard name={'Picture Frame'} price={100} />
+      <ProductCard name={'Example 2'} price={200} />
+      <ProductCard name={'Example 3'} price={300} />
+      <ProductCard name={'Example 4'} price={400} />
+      <ProductCard name={'Example 5'} price={500} />
+      <ProductCard name={'Example 6'} price={500} />
     </div>
   );
 }
