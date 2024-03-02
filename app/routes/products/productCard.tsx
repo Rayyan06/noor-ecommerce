2a7bc456f6c87fa6f7e62940b61c8e0122365c3f
Rayyan Khan <weatherwhiz06@gmail.com>
2024-03-02 18:43:34 -0500

    Added new page

--- app/routes/products/productCard.tsx
+++ app/routes/products/productCard.tsx
@@ -4,7 +4,13 @@ type ProductCardProps = {
 };
 export default function ProductCard({ name, price, image }: ProductCardProps) {
   return (
-    <div className="flex flex-col items-center justify-end">
+    <div
+      className="flex flex-col items-center justify-end"
+      style={{
+        backgroundImage:
+          'url("https://dummyimage.com/600x400/a6a6a6/000000.jpg&text=Product+Image")',
+      }}
+    >
       <h3 className="text-xl font-serif ">{name}</h3>
       <h3>${price}</h3>
     </div>
