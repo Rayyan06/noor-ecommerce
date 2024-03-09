33e764cd2ff2b252a6a575396166e535c168d60b
Rayyan Khan <weatherwhiz06@gmail.com>
2024-03-09 17:56:16 -0500

    Removed unnecessary imports

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,14 +1,10 @@
-import { LoaderFunctionArgs, json } from '@remix-run/node';
 import Card from './card';
-import { commitSession, getSession } from '~/sessions';
-import { useLoaderData } from '@remix-run/react';
-import { db } from '~/utils/db.server';
 
 export default function IndexRoute() {
   return (
     <div className="mx-36 space-y-10">
       <Card mainText="Browse our latest products" buttonText="Shop Now" />
-      <Card mainText="See more" buttonText="Shop Now" />
+      <Card mainText="See more" buttonText="Learn More" />
     </div>
   );
 }
